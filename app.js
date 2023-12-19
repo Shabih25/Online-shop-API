631df93bc3fc07abeeddfcfd7974787f65e79c08
shabih <shabih.nazir@indxtechnology.com>
2023-12-19 16:06:38 +0530

    Changes

--- app.js
+++ app.js
@@ -11,34 +11,6 @@ const app=express();
 const port=3000;
 //middlewares
 app.use(bodyParser.json());
-const cart = [];
-
-// Route to get the current cart items
-app.get('/cart', (req, res) => {
-  res.json(cart);
-});
-
-// Route to add a product to the cart
-app.post('/cart/add', (req, res) => {
-  const { productId, productName, price } = req.body;
-  const product = { productId, productName, price };
-
-  cart.push(product);
-  res.json({ message: 'Product added to cart', cart });
-});
-
-// Route to remove a product from the cart
-app.post('/cart/remove', (req, res) => {
-  const { productId } = req.body;
-  const index = cart.findIndex((item) => item.productId === productId);
-
-  if (index !== -1) {
-    cart.splice(index, 1);
-    res.json({ message: 'Product removed from cart', cart });
-  } else {
-    res.status(404).json({ error: 'Product not found in cart' });
-  }
-});
 const data=require('./data.json');
  
 app.use('/api',productRoutes);
