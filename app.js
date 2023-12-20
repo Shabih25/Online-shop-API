6d08934f85fd49813daa728c470d7c581eea840f
shabih <shabih.nazir@indxtechnology.com>
2023-12-20 09:09:52 +0530

    Changes

--- app.js
+++ app.js
@@ -12,9 +12,38 @@ const port=3000;
 //middlewares
 app.use(bodyParser.json());
 const data=require('./data.json');
-const cart=[];
 app.use('/api',productRoutes);
 app.use('/login', loginRouter);
+app.get('/api/cart',requireAuth,(req,res)=>{
+    res.json(req.user.cart);
+});
+
+//add product
+app.post('/api/cart/:productId',requireAuth,(req,res)=>{
+    const productId=parseInt(req.params.productId);
+    if (!req.user.cart) {
+        req.user.cart = []; 
+  }
+    const product=data.products.find(p=>p.id==productId);
+    if(!product){
+        res.status(404).json({error:'Product not found'});
+    }else{
+        req.user.cart.push(product);
+        res.json(req.user.cart);
+    }
+});
+
+//remove product
+app.delete('/api/cart/:productId',requireAuth,(req,res)=>{
+    const productId=parseInt(req.params.productId);
+    const index=req.user.cart.findIndex(p=>p.id===productId);
+    if (index===-1){
+        res.status(404).json({error:'Product not found in the cart'});
+    }else{
+        const removeProduct=req.user.cart.splice(index,1);
+        res.json(removeProduct[0]);
+    }
+})
 app.listen(port,()=>{
     console.log('Server is running on http://localhost:3000');
 });
